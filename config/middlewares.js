fcc590c675461254738c64ca1e939af0c78e76b0
DebasishDas97 <debasishdas1996.dd@gmail.com>
2025-04-09 13:34:18 +0530

    middleware changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,5 +1,18 @@
 module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173', // for local dev
+        'https://kohua-database-store.online', // your production domain
+        'https://www.kohua-database-store.online' // just in case
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      headers: '*',
+      credentials: true,
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
