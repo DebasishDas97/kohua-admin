26e95b4c6c4b41d47e3263b1bf511f0b43dd0d66
DebasishDas97 <debasishdas1996.dd@gmail.com>
2025-04-11 12:34:00 +0530

    chages

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('STRAPI_ADMIN_BACKEND_URL'),//update
+  // url: env('STRAPI_ADMIN_BACKEND_URL'),//update
   app: {
     keys: env.array('APP_KEYS'),
   },
